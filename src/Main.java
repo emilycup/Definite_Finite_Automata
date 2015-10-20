91261dcdac2076a2085d0f8038b6f8dcf555a681
emilycup <eemly.le@gmail.com>
2015-10-20 11:51:38 -0700

    String manipulation part done

--- src/Main.java
+++ src/Main.java
@@ -7,34 +7,39 @@
 // =============================================================================
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
-	static int numberOfStates = 0;
-	static int finalStates = 0;
-	static String[] alphabet;
-	static int i = 0;
-	static String transitions;
+	boolean accepted = false;
+	public int numberOfStates = 0;
+	public int finalStates = 0;
+	public String transitions;
+	public ArrayList<String> DFAInfo = new ArrayList();
 
 	public static void main(String[] args) throws IOException {
+		Main start = new Main();
 		String fileName = "src/language.txt";
-		readFile(fileName);
-		// displayFA();
+		start.readFile(fileName);
+		start.displayFA();
 	}
 
-	public static void readFile(String fileName) {
+	//	=============================================================================================
+	// 	READ FILE:
+	//  	Reads in and parses the .txt file for information needed
+	// ==============================================================================================
+	public void readFile(String fileName) {
 		String line = null;
 		// // reading in the file
 		try {
+			int i = 0;
 			FileReader fileReader = new FileReader(fileName);
 			BufferedReader buffReader = new BufferedReader(fileReader);
 			while ((line = buffReader.readLine()) != null) {
-				//alphabet[i] = line;
 				System.out.println(line);
+				DFAInfo.add(line);
 				i++;
 			}
 			buffReader.close();
@@ -47,7 +52,25 @@ public class Main {
 		}
 	}
 
-	public static void displayFA() {
-		System.out.println("Number of states:" + alphabet[0]);
+	//	=============================================================================================
+	// 	DISPLAY RESULTS:
+	//  	Will display the results and information about the Definite Finite Automata to the user
+	// ==============================================================================================
+	public void displayFA() {
+		int i = 3;
+		System.out.println("--- FINITE STATE AUTOMATION ---");
+		System.out.println("1) Number of states:" + DFAInfo.get(0));
+		System.out.println("2) Final states: " + DFAInfo.get(1));
+		System.out.println("3) Alphabet: " + DFAInfo.get(2).replace(" ", ", "));
+		System.out.println("4) Transitions: ");
+		while (DFAInfo.get(i).contains("(")){
+			System.out.println("\t\t" + DFAInfo.get(i).replace("(", "").replace(")", ""));
+			i++;
+		}
+		System.out.println("5) Strings: ");
+		while (i < DFAInfo.size()) {
+			System.out.println("\t\t" + DFAInfo.get(i));
+			i++;
+		}
 	}
 }
