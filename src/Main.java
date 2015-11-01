0b511bf9d366f7ffa587fa1c5ae8d1dd1f1918d6
ChoiByungWook <dchoi1@cpp.edu>
2015-11-01 02:16:11 -0800

    FA will now properly determine and output correctly whether an input is accepted or rejected. Need to work on testing empty string, separation of new FA rules.

--- src/Main.java
+++ src/Main.java
@@ -14,10 +14,19 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * The FA will properly determine whether or not an input is accepted or rejected.
+ *
+ * This FA will properly solve problems #1 and #2 on the hw. I will be working on #3 and #4 on Sunday.
+ * There are still a few problems regarding empty string testing, separation of the data within the .txt file.
+ * We need to create a FA again for each rule, which is determined by an empty line. In addition, I have updated the
+ * .txt and they will include the answers to #1-#4.
+ */
+
 public class Main {
     public boolean accepted = false;
     public int numberOfStates, state, finalValue;
-    public String finalStates, transitionInfo, alphabet, transitions;
+    public String finalStates, transitionInfo, alphabet;
     public ArrayList<String> DFAInfo = new ArrayList();
     public char[][] transitionTable;
     Set<Character> alphabetSet, finalstateset;
@@ -54,23 +63,22 @@ public class Main {
      */
     public boolean completeTransition(String word) {
         state = 0; // always will start at 0
-        finalValue = numberOfStates;
-
         for (int i = 0; i < word.length(); i++) {
             // will determine if the character is in the alphabet set
             if (!alphabetSet.contains(word.charAt(i))) {
+                System.out.println(i);
                 return false;
             } else {
                 state = Character.getNumericValue(transitionTable[state][Character.getNumericValue(word.charAt(i))]);
+//                System.out.println(Integer.toString(state));
             }
         }
 
-        if (finalstateset.contains(state)) {
+        if (finalstateset.contains(Character.forDigit(state, 10))) {
             return true;
         } else
             return false;
     }
-
 
 //	     state ← initial_state;   exit ← false;
 //	     while not exit do
@@ -88,33 +96,36 @@ public class Main {
 //	         end //if
 //	    end //while
 
-
     /**
      * SET VALUES:
      *      Will display the results and information about the Definite Finite Automata to the user
      */
     public void setValues() {
         int fileLine = 3;
-        numberOfStates = Integer.parseInt(DFAInfo.get(0));
 
-        alphabet = DFAInfo.get(2).replace(" ", "");
+        /**
+         * We will be using sets in order to keep track of the alphabet and final states.
+         * These sets will be used to compare and determine whether an input is accepted or rejected.
+         */
         alphabetSet = new HashSet<>();
-
-        // adding characters to the set
-        for (int i = 0; i < alphabet.length(); i++){
-            alphabetSet.add(alphabet.charAt(i));
-        }
-
-        // adding final states to the set
-        finalStates = DFAInfo.get(1).replace(" ", "");
         finalstateset = new HashSet<>();
 
-        for (int i = 0; i < finalStates.length(); i++) {
+        numberOfStates = Integer.parseInt(DFAInfo.get(0));
+
+        //Populating final states
+        finalStates = DFAInfo.get(1).replace(" ", "");
+        for(int i = 0; i < finalStates.length(); i++) {
             finalstateset.add(finalStates.charAt(i));
         }
 
+        //Populating alphabet states
+        alphabet = DFAInfo.get(2).replace(" ", "");
+        for(int i = 0; i < alphabet.length(); i++) {
+            alphabetSet.add(alphabet.charAt(i));
+        }
+
         // initialize transition table
-        transitionTable = new char[numberOfStates][alphabet.length()];
+        transitionTable = new char[numberOfStates][alphabetSet.size()];
 
         // fill transition table
         while (DFAInfo.get(fileLine).startsWith("(")) {
@@ -146,7 +157,15 @@ public class Main {
         }
         System.out.println("5) Strings: ");
         while (i < DFAInfo.size()) {
-            System.out.println("\t\t" + DFAInfo.get(i));
+            System.out.print("\t\t" + DFAInfo.get(i) + " ");
+
+            //We will be calling on our comparison function to determine whether the input is accepted or rejected
+            if(completeTransition(DFAInfo.get(i))) {
+                System.out.println("Accepted");
+            } else {
+                System.out.println("Rejected");
+            }
+
             i++;
         }
     }
@@ -157,10 +176,10 @@ public class Main {
      */
     public void printTransitionTable(){
         for (int i = 0; i < numberOfStates; i++){
-            for(int j = 0; j < alphabet.length(); j++){
-                System.out.println(transitionTable[i][j]);
+            for(int j = 0; j < alphabetSet.size(); j++){
+                System.out.print(transitionTable[i][j] + " ");
             }
-
+            System.out.print("\n");
         }
     }
 
@@ -175,7 +194,6 @@ public class Main {
         String fileName = "src/language.txt";
         start.readFile(fileName);
         start.setValues();
-        start.completeTransition("10");
         start.displayFA();
     }
 }
