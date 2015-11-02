1515c0fe0ff39795fcb399159bd42d995c946ead
ChoiByungWook <dchoi1@cpp.edu>
2015-11-01 16:16:12 -0800

    Lightly commented, still needs to clean and fixed. Deleted unused variables and functions.

--- src/Main.java
+++ src/Main.java
@@ -17,15 +17,11 @@ import java.util.Set;
 /**
  * The FA will properly determine whether or not an input is accepted or rejected.
  *
- * This FA will properly solve problems #1 and #2 on the hw. I will be working on #3 and #4 on Sunday.
- * There are still a few problems regarding empty string testing, separation of the data within the .txt file.
- * We need to create a FA again for each rule, which is determined by an empty line. In addition, I have updated the
- * .txt and they will include the answers to #1-#4.
+ * Optimize, works for problems 1-4.
  */
 
 public class Main {
-    public boolean accepted = false;
-    public int numberOfStates, state, finalValue;
+    public int numberOfStates, state;
     public String finalStates, transitionInfo, alphabet, fscopy, acopy;
     public ArrayList<String> DFAInfo = new ArrayList();
     public char[][] transitionTable;
@@ -40,7 +36,7 @@ public class Main {
      */
     public void readFile(String fileName) {
         String line = null;
-        // // reading in the file
+        // reading in the file
         try {
             int i = 0;
             FileReader fileReader = new FileReader(fileName);
@@ -64,54 +60,51 @@ public class Main {
     public boolean completeTransition(String word) {
         state = 0; // always will start at 0
         for (int i = 0; i < word.length(); i++) {
-            //N stands for a numeric value, which shall be theorized as 1 for the transition
 
+            //Checks for empty string and returns the initial state.
             if(word.trim().isEmpty()) {
                 state = 0;
             }
 
             else if(Character.isDigit(word.charAt(i))) {
+                //Z = Zero, along with P. Transition shall be marked as 0.
                 if(alphabetSet.contains('Z') && word.charAt(i) == '0') {
                     state = Character.getNumericValue(transitionTable[state][0]);
                 }
 
+                //N = Numbers 0-9, P = Postive 1-9. Transition will be marked as 1.
                 else if(alphabetSet.contains('N') || alphabetSet.contains('P')) {
                     state = Character.getNumericValue(transitionTable[state][1]);
                 }
 
+                //Otherwise we will check to see if the char is within the alphabet
                 else if(alphabetSet.contains(word.charAt(i))) {
                     state = Character.getNumericValue(transitionTable[state][Character.getNumericValue(word.charAt(i))]);
                 }
-
+                //Number doesn't exist within the alphabet
                 else {
                     return false;
                 }
             }
 
-            //L stands for an alphabetic value, which shall be theorized as 0 for the transition
-
+            //Checks for letters
             else if(Character.isAlphabetic(word.charAt(i))) {
+                //L stands for letters comprising of A-Z, a-z. Transition mark will be 0.
                 if(alphabetSet.contains('L')) {
                     state = Character.getNumericValue(transitionTable[state][0]);
                 }
-
+                //Letter doesn't exist within the alphabet
                 else {
                     return false;
                 }
             }
-
+            //Symbol doesn't exist within the alphabet
             else {
                 return false;
             }
-
-            // will determine if the character is in the alphabet set
-//            if (alphabetSet.contains(word.charAt(i))) {
-//                state = Character.getNumericValue(transitionTable[state][Character.getNumericValue(word.charAt(i))]);
-//            } else {
-//
-//            }
         }
 
+        //Check to see if the ending state is an accepted state.
         if (finalstateset.contains(Character.forDigit(state, 10))) {
             return true;
         } else
@@ -147,6 +140,7 @@ public class Main {
             numberOfStates = Integer.parseInt(DFAInfo.get(i));
             i++;
 
+            //string that will be used to print
             fscopy = DFAInfo.get(i);
             finalStates = DFAInfo.get(i).replace(" ", "");
             for(int j = 0; j < finalStates.length(); j++) {
@@ -154,6 +148,7 @@ public class Main {
             }
             i++;
 
+            //string that will be used to print
             acopy = DFAInfo.get(i);
             alphabet = DFAInfo.get(i).replace(" ", "");
             for(int j = 0; j < alphabet.length(); j++) {
@@ -161,6 +156,8 @@ public class Main {
             }
             i++;
 
+            //Initializing the adjacency matrix which will be used for transitioning within the states.
+            //The size was determined by having a combination of the states to the number of transitions
             transitionTable = new char[numberOfStates][alphabetSet.size()];
             k = i;
             while (DFAInfo.get(i).startsWith("(")) {
@@ -169,6 +166,8 @@ public class Main {
                 i++;
             }
 
+            //We are dividing our FAs by using an empty line, if we find an empty line then we will
+            //break and print out the results of the FA.
             while(i < DFAInfo.size()){
                 if(DFAInfo.get(i).isEmpty()) {
                     i++;
@@ -177,6 +176,10 @@ public class Main {
                 stringsToCheck.add(DFAInfo.get(i));
                 i++;
             }
+
+            //We need to clear these in order to allow for the computation of multiple FAs within one file.
+            //Otherwise it would be very difficult as we would be taking in multiple rules for multiple FAs
+            //which would make it a lot more complicated.
             displayFA(k);
             alphabetSet.clear();
             finalstateset.clear();
@@ -227,16 +230,15 @@ public class Main {
      * DISPLAY RESULTS:
      *      Will display the results and information about the Definite Finite Automata to the user
      */
-    public void displayFA(int i) {
-//        int i = 3;
+    public void displayFA(int Transitions) {
         System.out.println("--- FINITE STATE AUTOMATION ---");
         System.out.println("1) Number of states: " + numberOfStates);
         System.out.println("2) Final states: " + fscopy.replace(" ", ", "));
         System.out.println("3) Alphabet: " + acopy.replace(" ", ", "));
         System.out.println("4) Transitions: ");
-        while (DFAInfo.get(i).startsWith("(")) {
-            System.out.println("\t\t" + DFAInfo.get(i).replace("(", "").replace(")", ""));
-            i++;
+        while (DFAInfo.get(Transitions).startsWith("(")) {
+            System.out.println("\t\t" + DFAInfo.get(Transitions).replace("(", "").replace(")", ""));
+            Transitions++;
         }
         System.out.println("5) Strings: ");
         for (int j = 0; j < stringsToCheck.size(); j++) {
